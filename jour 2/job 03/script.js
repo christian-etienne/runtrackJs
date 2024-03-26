502c174412a91fcac0bcfb12cbe34f41c99d2d47
christian etienne <christian-michael.etienne@laplateforme.io>
2024-03-26 13:21:47 +0100

    modification du nom de la fonction

--- jour 2/job 03/script.js
+++ jour 2/job 03/script.js
@@ -1,6 +1,6 @@
 let compteur = 0;
 
-document.getElementById("button").addEventListener("click", function() {
-    compteur++;
-    document.getElementById("compteur").textContent = compteur;
-});
+document.getElementById("button").addEventListener("click", function addOne() {
+        compteur++;
+        document.getElementById("compteur").textContent = compteur;
+    });
