ba0b9a3d78a46b0144d94a4b50d6a359ca275ca9
christian etienne <christian-michael.etienne@laplateforme.io>
2024-03-26 15:07:55 +0100

    ajout job 4

--- jour 2/job 04/script.js
+++ jour 2/job 04/script.js
@@ -1,6 +1,11 @@
-let compteur = 0;
-
-document.getElementById("button").addEventListener("click", function() {
-    compteur++;
-    document.getElementById("compteur").textContent = compteur;
+document.addEventListener('keydown', function(event) {
+    let keylogger = document.getElementById('keylogger');
+    let key = event.key.toLowerCase();
+    if (key.length === 1 && key.match(/[a-z]/i)) {
+        if (document.activeElement === keylogger) {
+            keylogger.value += key + key;
+        } else {
+            keylogger.value += key;
+        }
+    }
 });
