3696809984bc98b72ab16ad20cd37e5b0396797a
christian etienne <christian-michael.etienne@laplateforme.io>
2024-03-27 11:31:23 +0100

    ajout job 1

---
+++ jour 3/job 1/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $("#show").click(function(){
+      $("#phrase").show()
+    });
+
+    $("#hide").click(function(){
+      $("#phrase").hide()
+    });
+    
+});
